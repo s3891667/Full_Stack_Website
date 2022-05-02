953e38109dacbe37042e8ee3adc7724483f5ae24
hoangpham38914555 <s3891455@rmit.edu.vn>
2022-05-03 00:41:06 +0700

    Update Login section

--- login.php
+++ login.php
@@ -4,14 +4,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/bootstrap.css">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./login.css">
     <title>InstaKilogram</title>
 </head>
 <body>
-  <section class="container-fluid">
-    <section class="row justify-content-center">
-      <section class="col-12 col-sm-6 col-md-3">  
-          <form action="data_check.php" class="form-container" method="post">
+  <section class="form_box">
+      <section class="container">  
+          <form action="data_check.php" class="form-horizontal" method="post">
               <h1 class="text-center">Login</h1>
             <div class="form-group ">
               <label for="inputEmail3" class="col-form-label">Email</label>
@@ -28,17 +27,16 @@
               </label>
             </div>  
             <div class ="d-grid gap-2 mt-3">
-              <button type="submit" onclick="" class="btn btn-primary" name="signIn" >Sign in</button>
+              <button type="submit" onclick="" class="btn btn-dark button_des" name="signIn" >Sign in</button>
             </div>
             <div class="text-center mt-3">
-              <a href="signUp.html" >Sign up for InstaKilogram </a>
-              <p class="d-inline">  .  </p>
-              <a href="forgotpassword.html">Forgotten your password ?</a>
+              <a href="signUp.html" class="text_des">Sign up for InstaKilogram </a>
+              <p class="d-inline"> .  </p>
+              <a href="forgotpassword.html" class="text_des">Forgotten your password ?</a>
             </div>
           </form>
       </section>
     </section>
-  </section>   
-</body>
+    </body>
 </html>
 
