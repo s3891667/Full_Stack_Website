c1af318506c4777d0745a481dc6a6611a6c7d9a4
hoangpham38914555 <s3891455@rmit.edu.vn>
2022-05-08 17:10:45 +0700

    update

--- home.php
+++ home.php
@@ -4,107 +4,96 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="cookies_content.css">   
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>Homepgae</title>
 </head>
+
 <body>
     <header>
-        <!-- Logo Image  -->
         <div>
-            <img class="img" src="picture/Logo_picture.jpg" alt="logo">
-        </div> 
-        <div class="webheading">
-            <!-- Website Heading -->
-            <h1>InstaKilogram</h1>
-            <p>Welcome <?php echo $_SESSION["user"]; ?> to InstaKilogram</p>
-            <span>Inspire The World By Yourself</span>
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">InstaKilogram</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+
+                        <li><a href="#">TRENDING POST</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-        <div class="nav_background">
-            <!-- Navigation Bar -->
-            <a class="nav nav_setcolor" href="./index.html">Homepage</a>
-            <a class="nav nav_setcolor" href="#">Trending Post</a>
-            <a class="nav nav_setcolor" href="./login.html">Log In</a>
-            <a class="nav nav_setcolor" href="./logout.php">Logout</a>
-            <a class="nav nav_setcolor"  href="./user_profile.php">Profile</a>
+
+        <div class="container">
+            <form action="/action_page.php">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search" name="search">
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><i
+                                class="glyphicon glyphicon-search"></i></button>
+                    </div>
+                </div>
+            </form>
         </div>
-            <!-- Search Bar -->
-        <div>
-            <input class="searchbar" type="text" placeholder="Search here...">
+
+        <div class="img_dess">
+            <img class="img_des" src="./picture/imgconnect.jpg" alt="intropic">
         </div>
     </header>
     <main>
-        <div class="overallpost">
-            <!-- Overall Post -->
-            <div class="ava_user">
-                <!-- icon and user name -->
-                <img src="picture/ava.jpeg" alt="Avatar" class="avatar">
-                <span class="username">
-                    Dang Khoa
-                </span>
-            </div>
-            <div class="picture_post">
-                <!-- Picture -->
-                <img src="picture/image1.jpeg" alt="post">
-            </div>
-            <div>
-                <!-- Icon: like, comment -->
-                <span>
-                    <!-- Like -->
-                    <img src="picture/likeicon.png" alt="likeicon" class="avatar">
-                </span>
-                <span>
-                    <!-- Comment -->
-                    <img src="picture/commenticon.webp" alt="comment" class="avatar2">
-                </span>
-            </div>
-            <div>
-                <!-- Number of like -->
-                <div>
-                    <p>
-                        <qoute>Today is not an easy day</qoute>
-                    </p>
-                </div>
-            </div>
-            <div>
-                <!-- Caption -->
-            </div>
-            <div>
-                <!-- View comment -->
-            </div>
-            <div>
-                <!-- Data time -->
-            </div>
+    <section>
+        <!-- Post -->
+        <div>
+            <!-- Logo and Username-->
+            <span>
+                <!-- Logo -->
+                
+            </span>
+            <span>
+                <!-- Username -->
+            </span>
         </div>
+        <div>
+            <!-- Picture -->
+        </div>
+        <div>
+            <!-- Caption -->
+        </div>
+        <div>
+            <!-- Like -->
+            <span>
+                <!-- Like -->
+            </span>
+            <span>
+                <!-- Share Button -->
+            </span>
+        </div>
+    </section>
     </main>
-    <aside>
-        
+    <<aside>
+        <!-- People follow-->
     </aside>
     <footer>
-    <div>
-        <a class="footer_tab" href="#">About Us</a>
-        <a class="footer_tab" href="#">Copyright</a>
-        <a class="footer_tab" href="#">Privacy</a>
-        <a class="footer_tab" href="#">Contact Information</a>
-    </div>
+        <div>
+            <a class="footer_tab" href="#">About Us</a>
+            <a class="footer_tab" href="#">Copyright</a>
+            <a class="footer_tab" href="#">Privacy</a>
+            <a class="footer_tab" href="#">Contact Information</a>
+        </div>
     </footer>
-    
-    <!--Cookie black screen-->
-    <div class="wrapper"></div>
+</body>
 
-    <!--Cookie consent popup-->
-    <div class="cookie-container">
-        <div>I use cookies</div> 
-        <p>My website uses cookies necessary for its basic <br>functioning. By continuing browsing, you consent <br>to my use of cookies and other technologies.
-        </p>
-        <button class="accept-button">I understand</button>
-        <a href="#">Learn more</a>
-    </div>
-
+</html>
     <!--Cookie javascript file-->
     <script src="cookies_content.js"></script>
 </body>
