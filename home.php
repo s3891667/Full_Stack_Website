7b90d1c0b21f045cf4377abb826c3ee28a567bab
s3891667 <s3891667@rmit.edu.vn>
2022-05-11 14:16:51 +0700

    implementing user resources

--- home.php
+++ home.php
@@ -35,10 +35,10 @@ if (!isset($_SESSION['user'])) {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link text-white" href="#">Sign Up</a>
+          <a class="nav-link text-white" href="./user_profile.php">Profile</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white" href="#">Log In</a>
+          <a class="nav-link text-white" href="./logout.php">Logout</a>
         </li>
       </ul>
       <form class="d-flex">
