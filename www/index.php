9907b353d95a3c91a3ef1b924a2794e656f34401
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2022-05-17 12:57:03 +0700

    edit cookie in index.php

--- www/index.php
+++ www/index.php
@@ -21,7 +21,6 @@ if (isset($_SESSION['user'])) {
 </head>
 
 <script>
-
   // this js will display text on scroll 
   window.onscroll = function() {
     myFunction()
@@ -38,7 +37,7 @@ if (isset($_SESSION['user'])) {
 
 
 
-<body>    
+<body>
   <header>
     <nav id="colorsetup1" class="navbar navbar-expand-lg navbar-light">
       <div class="container-fluid">
@@ -149,15 +148,17 @@ if (isset($_SESSION['user'])) {
       <a class="footer_tab" href="#">Contact Information</a>
     </div>
   </footer>
-  <div class="wrapper"></div>
-    <div class="cookie-container">
-        <div>I use cookies</div>
-        <p>My website uses cookies necessary for its basic <br>functioning. By continuing browsing, you consent <br>to
+  <?php
+  echo "<div class='wrapper'></div>";
+  echo "<div class='cookie-container'>";
+  echo     "<div>I use cookies</div>";
+  echo   "<p>My website uses cookies necessary for its basic <br>functioning. By continuing browsing, you consent <br>to
             my use of cookies and other technologies.
-        </p>
-        <button class="accept-button">I understand</button>
-        <a href="#">Learn more</a>
-    </div>
+        </p>";
+  echo "<button class='accept-button'>I understand</button>";
+  echo "<a href='#'>Learn more</a>";
+  echo "</div>";
+  ?>
 </body>
 
 </html>
